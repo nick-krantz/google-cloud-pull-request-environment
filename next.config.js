6d5d7f9674fce28ac040d6c7575a37b82fa8d5e4
Nick Krantz <nick@krantz.app>
2022-12-03 18:50:12 -0600

    broke build with my bad configuration

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  target: 'serverless',
-  assetPrefix: isProd ? '' : '',
 }
 
 module.exports = nextConfig
